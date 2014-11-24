f364da233ef47a4cd0129c93efcc5427f401854d
Sachin Rudraraju <sachin.rudraraju@gmail.com>
2014-11-24 18:02:53 -0500

    all parts working

--- clustering.hs
+++ clustering.hs
@@ -1,6 +1,7 @@
 import qualified Data.ByteString.Char8 as B
 import Debug.Trace
-import Data.List
+import Data.List as L
+import Data.Set as S
 
 type Entity = [Char]
 type Property = Int
@@ -14,8 +15,11 @@ simMatrix c p el = [[distance c p e1 e2 | e1 <- el] | e2 <- el]
 -- this version copies over fold so you don't have to do distance twice
 -- it's kind of ugly
 simMatrix :: DBC -> Property -> [Entity] -> [[Double]]
+{-
 simMatrix c p el = copyOver [[ if j > i then distance c p (el !! i) (el !! j) else 0 | j <- eindex] | i <- eindex]
     where   eindex = [0..length el - 1]
+-}
+simMatrix c p el = [[0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 10.0, 0.0, 0.0, 0.0], [0.0, 10.0, 0.0, 10.0, 0.0, 10.0], [0.0, 0.0, 10.0, 0.0, 5.0, 0.0], [0.0, 0.0, 0.0, 5.0, 0.0, 0.0],[0.0, 0.0, 10.0, 0.0, 0.0, 0.0]]
 
 copyOver :: [[Double]] -> [[Double]]
 copyOver sm = [[ if j < i then sm !! j !! i else sm !! i !! j | j <- eindex] | i <- eindex]
@@ -24,27 +28,25 @@ copyOver sm = [[ if j < i then sm !! j !! i else sm !! i !! j | j <- eindex] | i
 
 -- find subtree from a starting point
 -- frontier + list of nodes to go to
-searchTree :: [Int] -> Double -> [[Double]] -> [Int] -> [[Int]] -> Int -> [[Int]]
-searchTree visit d sm found acc 1 | trace ("search: " ++ show visit ++ " " ++ show found) False = undefined
-searchTree (s:visit) d sm found acc n = 
+{-
+searchTree visit d sm found acc | trace ("search: " ++ show visit ++ " " ++ show found) False = undefined
+-}
+searchTree :: [Int] -> Double -> [[Double]] -> [Int] -> [[Int]] -> [[Int]]
+searchTree (s:visit) d sm found acc = 
     let eindex = [0..length sm - 1]
         -- so has to be connected and not found already, or already a part of visit
-        nvisit = filter (\x -> (d < sm !! s !! x && x `notElem` found) || x `elem` visit) eindex
+        nvisit = L.filter (\x -> (d < sm !! s !! x && x `notElem` found) || x `elem` visit) eindex
     in  if length nvisit == 0 then 
-            if alreadyFound (s:found) acc then acc -- return condition
+            if compare1d2d (s:found) acc then acc     -- return condition
             else (s:found):acc                        -- add this result onto acc
-        else searchTree nvisit d sm (s:found) acc 0   -- go to next node
-
-alreadyFound :: (Eq a) => [a] -> [[a]] -> Bool
-alreadyFound x [] = False
-alreadyFound x (y:ys) = null (x \\ y) || alreadyFound x ys
+        else searchTree nvisit d sm (s:found) acc     -- go to next node
 
 {- 
 allConnected d sm = [searchTree [s] d sm [] | s <- eindex]
 -}
 -- find all subtrees
 allConnected :: Double -> [[Double]] -> [[Int]]
-allConnected d sm = foldl (\acc x -> searchTree [x] d sm [] acc 0) [] eindex
+allConnected d sm = L.foldl (\acc x -> searchTree [x] d sm [] acc) [] eindex
     where eindex = [0..length sm - 1]
 
 {-
@@ -56,9 +58,31 @@ thresholdClustering :: DBC -> Property -> [Entity] -> [[[Int]]]
 thresholdClustering c p el = 
 	let diff acc d = 
 		let clusters = allConnected d (simMatrix c p el)
-		in	if head acc == clusters then acc 	-- head because 
-				else clusters:acc 				-- we are adding on to the head here
-	in 	foldl diff [[[]]] [10, 20..500]
+		in	if compare2d2d (head acc) clusters then acc -- discard if same cluster
+				else clusters:acc -- we are adding on to the head here
+	in 	init $ L.foldl diff [[]] [0,2..10]
 
 distance :: DBC -> Property -> Entity -> Entity -> Double
 distance c p e1 e2 = 5
+
+
+
+
+
+{-
+compare1d2d x (y:ys) | trace ("1d: " ++ show x ++ " " ++ show y) False = undefined
+compare1d2d x (y:ys) = null (x \\ y) || compare1d2d x ys
+-}
+-- should give false if they are not equal, true if equal
+compare1d2d :: (Eq a, Show a) => [a] -> [[a]] -> Bool
+compare1d2d _ [] = False
+compare1d2d x (y:ys) = L.null (x L.\\ y) || compare1d2d x ys
+
+{- 
+compare2d2d xs [] = False 
+compare2d2d xs (y:ys) = compare1d2d y xs || compare2d2d xs ys
+compare2d2d xs ys | trace ("compare: " ++ show xs ++ " " show ys) False = undefined
+-}
+compare2d2d :: (Ord a, Eq a, Show a) => [[a]] -> [[a]] -> Bool
+compare2d2d xs ys = set2d xs == set2d ys
+    where set2d list = S.fromList [S.fromList l | l <- list]
